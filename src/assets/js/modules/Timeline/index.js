3d12aafda2855c99c806dcd53a0cd86c017a07e4
Laurent Menu <menu.laurent@gmail.com>
2016-12-27 18:05:51 +0100

    Rework timeline

--- src/assets/js/modules/Timeline/index.js
+++ src/assets/js/modules/Timeline/index.js
@@ -53,55 +53,58 @@ export default class Timeline extends React.Component {
 			</div>
 
 			<div className="desktop">
-				{Object.keys(this.props.views).map((step, stepIndex) => {
-					var title, current = false
+				<div className="back"><a href="/" onClick={this.handleClick}><i></i>Expéditions</a></div>
+				<div className="steps">
+					{Object.keys(this.props.views).map((step, stepIndex) => {
+						var title, current = false
 
-					switch(step) {
-						case 'preparation': 
-							title = 'Préparation'
-							break
-						case 'exploration':
-							title = 'Exploration'
-							break
-						case 'retour':
-							title = 'Retour'
-							break
-					}
+						switch(step) {
+							case 'preparation': 
+								title = 'Préparation'
+								break
+							case 'exploration':
+								title = 'Exploration'
+								break
+							case 'retour':
+								title = 'Retour'
+								break
+						}
 
-					if (step == this.props.currentStep) current = true
+						if (step == this.props.currentStep) current = true
 
-					return (
-						<div className={"step " + (current ? "current" : "")} style={{width: 100 / Object.keys(this.props.views).length + "%"}}>
-							<h4>{title}</h4>
-							<h3>{title}</h3>
+						return (
+							<div className={"step " + (current ? "current" : "")} style={{width: 100 / Object.keys(this.props.views).length + "%"}}>
+								<h3>{title}</h3>
 
-							<div className="line">
-								{this.props.views[step].map((view, index) => {
-									var current = false
-									var passed = true
+								<div className="line">
+									{this.props.views[step].map((view, index) => {
+										var current = false
+										var passed = true
 
-									if (view.slug == this.props.currentPage) {
-										current = true
-										alreadyPassedItem = true
-									}
+										if (view.slug == this.props.currentPage) {
+											current = true
+											alreadyPassedItem = true
+										}
 
-									if (alreadyPassedItem) {
-										passed = false
-									}
-									
-									return (
-										<div className={(current ? "current" : "") + " " + (passed ? "passed" : "")} style={{left: ((100 / (this.props.views[step].length - 1)) * index) + "%"}}>
-											<a href={"/expedition/" + this.props.slug + "/" + view.slug} onClick={this.handleClick}>
-												<i></i>
-												<span>{view.title}</span>
-											</a>
-										</div>
-									)
-								})}
+										if (alreadyPassedItem) {
+											passed = false
+										}
+										
+										return (
+											<div className={(current ? "current" : "") + " " + (passed ? "passed" : "")} style={{left: ((100 / (this.props.views[step].length - 1)) * index) + "%"}}>
+												<a href={"/expedition/" + this.props.slug + "/" + view.slug} onClick={this.handleClick}>
+													<i></i>
+													<span>{view.title}</span>
+												</a>
+											</div>
+										)
+									})}
+								</div>
 							</div>
-						</div>
-					)
-				})}
+						)
+					})}
+					<div className="clearfix"></div>
+				</div>
 				<div className="clearfix"></div>
 			</div>
 		</div>
